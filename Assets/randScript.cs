af5d101bc844a98d76687beb61bcbb30047509fb
satyadev <satyagames@gmail.com>
2017-03-27 18:11:11 +0530

    first commit

---
+++ Assets/randScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class randScript : MonoBehaviour {
+	
+	public Text displayText;
+	public int oldRandNum;
+	public int randNum;
+	void Start()
+	{
+		randNum = 0;
+		oldRandNum = 0;
+	}
+
+	public void ButtonClick()
+	{
+		randNum = Random.Range (1, 6);
+		if (oldRandNum != randNum) 
+		{
+			displayText.text = randNum.ToString ();
+		}
+		else 
+		{
+			ButtonClick ();
+		}
+		oldRandNum = randNum;
+	}
+}
